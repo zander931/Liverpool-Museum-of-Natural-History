cf9fa35232ef426d68ba0777f3659febfd0b8153
zander931 <zanderackevic@icloud.com>
2025-01-13 20:07:16 +0000

    Error handling for consumer

--- consumer.py
+++ consumer.py
@@ -15,31 +15,43 @@ def consume_messages(cons: Consumer, messages_consumed=0) -> None:
     try:
         while messages_consumed < MAX_MESSAGES:
             msg = cons.poll(1.0)
+            if msg is None:
+                logging.debug("No new message received within the timeout.")
             if msg:
                 if msg.error():
+                    logging.debug("There is an error.")
                     raise KafkaException(msg.error())
                 else:
                     messages_consumed += 1
                     if messages_consumed % log_frequency == 0:
-                        at = json.loads(msg.value().decode().get('at'))
-                        site = json.loads(msg.value().decode().get('site'))
-                        val = json.loads(msg.value().decode().get('val'))
-                        if int(val) == -1:
-                            type = json.loads(msg.value().decode().get('type'))
-                            if type is None:
+                        try:
+                            at = json.loads(msg.value().decode()).get('at')
+                            site = json.loads(msg.value().decode()).get('site')
+                            val = json.loads(msg.value().decode()).get('val')
+                            logging.debug("Data retrieved.")
+
+                            if val is None:
                                 logging.error(
                                     "Mechanical error at offset '{0}'".format(msg.offset()))
+                            elif not isinstance(val, int):
+                                if not val.isdigit():
+                                    logging.debug("Missing data in event.")
+                            elif int(val) == -1:
+                                type = json.loads(
+                                    msg.value().decode()).get('type', None)
+                                if type is None:
+                                    logging.error(
+                                        "Mechanical error at offset '{0}'".format(msg.offset()))
+                                else:
+                                    logging.info("Message {0} REQUEST: at={1}, site={2}, type={3}".format(
+                                        messages_consumed, at, site, type))
                             else:
-                                logging.info("Message {messages_consumed} REQUEST: at {1}, site {2}, type: {3}".format(
-                                    messages_consumed, at, site, type))
-                        else:
-                            logging.info("Message {0} RATING: at {1}, site {2}, val: {3}".format(
-                                messages_consumed, at, site, val))
-                            # print(json.loads(msg.value().decode(), msg.offset(), msg.error()))
+                                logging.info("Message {0} RATING: at={1}, site={2}, val={3}".format(
+                                    messages_consumed, at, site, val))
 
-                        if not msg.value():
-                            logging.warning(
-                                "\nEmpty message received at offset {0}".format(msg.offset()))
+                        except AttributeError as e:
+                            logging.debug(f"Logging data raised an error: {e}")
+                            continue
 
         logging.info(
             "Finished consuming {0} messages.".format(messages_consumed))
@@ -59,7 +71,7 @@ if __name__ == '__main__':
     log_frequency = 20
 
     load_dotenv()
-    setup_logging('console')
+    setup_logging('console', level=10)
 
     kafka_config = {
         'bootstrap.servers': ENV['BOOTSTRAP_SERVERS'],
@@ -67,10 +79,11 @@ if __name__ == '__main__':
         'sasl.mechanisms': ENV['SASL_MECHANISM'],
         'sasl.username': ENV['USERNAME'],
         'sasl.password': ENV['PASSWORD'],
-        'group.id': 'zander-cg',
+        'group.id': 'zander-cg6',
         'auto.offset.reset': 'earliest'
     }
 
     consumer = Consumer(kafka_config)
+    logging.debug("Subscribing to topic: {0}".format(ENV["TOPIC"]))
     consumer.subscribe([ENV["TOPIC"]])
     consume_messages(consumer)
